472f06537a5b40a97dbbfddd64d12f7d00b6cb4e
4K SANZZ <113883954+4ksanzz@users.noreply.github.com>
2022-11-22 19:55:16 +0800

    Update config.js

--- config.js
+++ config.js
@@ -10,39 +10,39 @@
 //Link Plugins (https://github.com/BOTCAHX/RTXZY-MD/blob/v3.0/plugins/info-creator.js)
 
 //Global Owner//
-global.owner = ['6281395861695']  // isi jika perlu
-global.mods = ['6281395861695']   // isi jika perlu
-global.prems = ['6281395861695']  // isi jika perlu
+global.owner = ['6281236167286']  // isi jika perlu
+global.mods = ['6281236167286']   // isi jika perlu
+global.prems = ['6281236167286']  // isi jika perlu
 
 //Nomor Owner & payment//
-global.nameowner = 'TioXd' // isi nama kalian bebas
-global.numberowner = '6281395861695' // isi ini untuk mengendalikan bot, untuk mengubah tampilan owner ada di plugins 
-global.dana = '6281395861695' // isi dengan nomor dana yg kamu punya 
-global.pulsa = '6281395861695' // isi dengan nomor pulsa hp kalian
-global.gopay = '6281395861695'
+global.nameowner = 'Sanzz' // isi nama kalian bebas
+global.numberowner = '6281236167286' // isi ini untuk mengendalikan bot, untuk mengubah tampilan owner ada di plugins 
+global.dana = '6281236167286' // isi dengan nomor dana yg kamu punya 
+global.pulsa = '6281236167286' // isi dengan nomor pulsa hp kalian
+global.gopay = '6281236167286'
 
 //Nama Bot & Grouplink//
-global.namebot = 'RTXZY-MD'
-global.gc = 'https://chat.whatsapp.com/D6BZVRSymWaFU28t2lxaO0'
-global.web = 'https://github.com/BOTCAHX' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
-global.instagram = 'https://instagram.com/prm2.0'
+global.namebot = 'R.SZ-MD'
+global.gc = 'https://chat.whatsapp.com/HHbTWShvfQO4Kep09NGUFr'
+global.web = 'https://youtube.com/@4KSanzz' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.instagram = 'https://instagram.com/4k.sanzz'
 
 //Tampilan wm Bot dan pesan bot//
 global.lolkey = 'rey2k22' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = '© R-TXZY-MD'
+global.wm = '© R.SZ-MD'
 global.watermark = wm
-global.wm2 = '⫹⫺ R-TXZY-MD'
-global.wm3 = '© R-TXZY-MD'
-global.wm4 = '© 2021/2022'
+global.wm2 = '⫹⫺ R.SZ-MD'
+global.wm3 = '© R.SZ-MD'
+global.wm4 = '© R. SZ-MD'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text='
 global.wait = '_*Tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
-global.packname = 'Made with'
-global.author = 'Bot WhatsApp\nR-TXZY-MD'
+global.packname = '𝐑. 𝐒𝐙'
+global.author = '√'
 
 //Apikey Setting//
 global.APIs = { // API Prefix
